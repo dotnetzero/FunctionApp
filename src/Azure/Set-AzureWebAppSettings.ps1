6295b8e3bc17fad718bdd75f4d9362b789b61af4
Eric Williams <ericwilliams76@gmail.com>
2018-01-19 11:24:20 -0700

    Adds option to merge or replace appsettings

--- src/Azure/Set-AzureWebAppSettings.ps1
+++ src/Azure/Set-AzureWebAppSettings.ps1
@@ -3,15 +3,52 @@ function Set-AzureWebAppSettings {
     param (
         [parameter(ValueFromPipeline)][HashTable]$Appsettings,
         [string]$ResourceGroupName,
-        [string]$WebAppName
+        [string]$WebAppName,
+        [switch]$Replace = $false
     )
 
     process {
 
-        if ($Appsettings.Count -gt 0) {
-            Write-Verbose "Set $($Appsettings.Count) app settings for $($ResourceGroupName) / $($WebAppName)"
-            Set-AzureRmWebApp -ResourceGroupName $ResourceGroupName -Name $WebAppName -AppSettings $Appsettings
+        if ($Appsettings.Count -eq 0) {
+            Write-Verbose "No appsettings passed!"
+            return
         }
 
+        if ($Replace) {
+            Write-Verbose "Replacing app settings for $($ResourceGroupName) / $($WebAppName)"
+            Set-AzureRmWebApp -ResourceGroupName $ResourceGroupName -Name $WebAppName -AppSettings $Appsettings
+            return;
+        }
+
+        Write-Verbose "Getting app settings for $($ResourceGroupName) / $($WebAppName)"
+        $webApp = Get-AzureRmWebApp -ResourceGroupName $ResourceGroupName -Name $WebAppName
+
+        $newAppSettings = @{}
+
+        $webApp.SiteConfig.AppSettings | ForEach-Object {
+        
+            Write-Verbose "Searching incoming app settings for key $($_.Name)"            
+            $result = $Appsettings[$_.Name]
+
+            if ($result -eq $null) {
+                Write-Verbose "Ignore: Key $($_.Name) does not existing in the incoming app settings"
+                $newAppSettings.Add($_.Name, $_.Value)
+            }
+            else {
+                Write-Verbose "Update: Key $($_.Name) exists in the incoming app settings as $result"
+                $newAppSettings.Add($_.Name, $result)
+            }
+
+        }
+    
+        $Appsettings.GetEnumerator() |`
+            Where-Object { -Not $newAppSettings.Contains($_.Key) } |`
+            ForEach-Object { 
+            Write-Verbose "Create: Key $($_.Name) does not exists in the remote app settings"
+            $newAppSettings.Add($_.Key, $_.Value)  
+        }
+    
+        Set-AzureRmWebApp -ResourceGroupName $ResourceGroupName -Name $WebAppName -AppSettings $newAppSettings
+        
     }
 }
